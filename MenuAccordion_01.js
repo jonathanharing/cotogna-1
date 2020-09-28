89af1a2aa7bb87bdd34aa1a63dd3ebebf96b7ec0
jonathanharing <47490021+jonathanharing@users.noreply.github.com>
2020-09-28 13:27:43 -0700

    Update MenuAccordion_01.js

--- MenuAccordion_01.js
+++ MenuAccordion_01.js
@@ -1,22 +1,24 @@
 // Establish variables
-alert("WTF!");
-var foodMenus = $('#foodmenus').children('.foodmenus-collection-item');
-var menuCount = foodMenus.length;
-console.log("menu count: " + menuCount);
+$(document).ready(function() {
+	alert("WTF!");
+	var foodMenus = $('#foodmenus').children('.foodmenus-collection-item');
+	var menuCount = foodMenus.length;
+	console.log("menu count: " + menuCount);
 
-// Data + accessibility setup
-foodMenus.each(function (index){
-	var accordionContentID = "accordion-" + index;
-	console.log(accordionContentID);
-	$(this).data('menu-id', index);
-	// Accessibility labels + controls
-	$(this).children('.foodmenus__accordion-button').attr('id', accordionContentID);
-	$(this).children('.foodcontent__grid').attr('aria-labelledby', accordionContentID);
-	if (index == 0) {
-		$(this).children('.foodmenus__accordion-button').attr('aria-expanded', 'true');
-		$(this).children('.foodmenus__accordion-button').attr('aria-disabled', 'true');
-	} else {
-		$(this).children('.foodmenus__accordion-button').attr('aria-expanded', 'false');
-		$(this).children('.foodmenus__accordion-button').attr('aria-disabled', 'false');
-	}
-});
+	// Data + accessibility setup
+	foodMenus.each(function (index){
+		var accordionContentID = "accordion-" + index;
+		console.log(accordionContentID);
+		$(this).data('menu-id', index);
+		// Accessibility labels + controls
+		$(this).children('.foodmenus__accordion-button').attr('id', accordionContentID);
+		$(this).children('.foodcontent__grid').attr('aria-labelledby', accordionContentID);
+		if (index == 0) {
+			$(this).children('.foodmenus__accordion-button').attr('aria-expanded', 'true');
+			$(this).children('.foodmenus__accordion-button').attr('aria-disabled', 'true');
+		} else {
+			$(this).children('.foodmenus__accordion-button').attr('aria-expanded', 'false');
+			$(this).children('.foodmenus__accordion-button').attr('aria-disabled', 'false');
+		}
+	});
+}
