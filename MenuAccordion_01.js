ff94a299e192010ebbd73cd4e0de634a652f9820
jonathanharing <47490021+jonathanharing@users.noreply.github.com>
2020-09-28 12:40:42 -0700

    Update MenuAccordion_01.js

--- MenuAccordion_01.js
+++ MenuAccordion_01.js
@@ -1,1 +1,19 @@
-$('.h1--alt').hide();
+// Establish variables
+var foodMenus = $('#foodmenus').children('.foodmenus-collection-item');
+var menuCount = foodMenus.length;
+
+// Data + accessibility setup
+foodMenus.each(function (index){
+	var accordionContentID = "accordion-" + index;
+	$(this).data('menu-id', index);
+	// Accessibility labels + controls
+	$(this).children('.foodmenus-accordion-button').attr('id', accordionContentID);
+	$(this).children('.foodcontent__grid').attr('aria-labelledby', accordionContentID);
+	if (index == 0) {
+		$(this).children('.foodmenus-accordion-button').attr('aria-expanded', 'true');
+		$(this).children('.foodmenus-accordion-button').attr('aria-disabled', 'true');
+	} else {
+		$(this).children('.foodmenus-accordion-button').attr('aria-expanded', 'false');
+		$(this).children('.foodmenus-accordion-button').attr('aria-disabled', 'false');
+	}
+});
